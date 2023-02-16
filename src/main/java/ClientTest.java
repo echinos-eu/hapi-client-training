f0747d1b925ec7942d15e784af8821f382980c49
patrick-werner <pa.f.werner@gmail.com>
2023-02-16 09:34:16 +0100

    initial state

--- src/main/java/ClientTest.java
+++ src/main/java/ClientTest.java
@@ -16,14 +16,5 @@ public class ClientTest {
 
     // Create a client
     IGenericClient client = ctx.newRestfulGenericClient("https://hapi.fhir.org/baseR4");
-
-    // Read a patient with the given ID
-    Patient patient = client.read().resource(Patient.class).withId("example").execute();
-
-    // Print the output
-    String string = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(patient);
-    System.out.println(string);
-
   }
-
 }
