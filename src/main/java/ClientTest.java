71816be64939dbf44a8985401ec4e8f9ef2cb353
patrick-werner <pa.f.werner@gmail.com>
2023-02-16 14:06:29 +0100

    search Patient

--- src/main/java/ClientTest.java
+++ src/main/java/ClientTest.java
@@ -2,6 +2,8 @@
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
 public class ClientTest {
@@ -16,5 +18,18 @@ public class ClientTest {
 
     // Create a client
     IGenericClient client = ctx.newRestfulGenericClient("https://hapi.fhir.org/baseR4");
+    //add logging interceptor
+    client.registerInterceptor(new LoggingInterceptor());
+
+    // search for Patient "Smith"
+    Bundle returnBundle = client.search()
+        .forResource(Patient.class)
+        .where(Patient.NAME.matches().value("Smith"))
+        .returnBundle(Bundle.class)
+        .execute();
+
+    returnBundle.getEntry().stream().map(e -> e.getResource())
+        .forEach(r -> System.out.println("ResourceId: " + r.getId()));
+    System.out.println("Found a total of: " + returnBundle.getTotal());
   }
 }
