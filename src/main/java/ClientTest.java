3edd83ad0a33ffaa45227ff3bd8c40ee4682f883
patrick-werner <pa.f.werner@gmail.com>
2023-09-19 13:40:48 +0200

    bundle sout working

--- src/main/java/ClientTest.java
+++ src/main/java/ClientTest.java
@@ -1,6 +1,7 @@
 
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.api.SearchTotalModeEnum;
@@ -23,10 +24,29 @@ public class ClientTest {
   public static void main(String[] args) {
 
     // Create a context
-    FhirContext ctx = FhirContext.forR4();
+    FhirContext ctx = FhirContext.forCached(FhirVersionEnum.R4);
 
     // increase socketTimeout
     ctx.getRestfulClientFactory().setSocketTimeout(20 * 1000);
+
+    String baseUrl = "https://hapi.fhir.org/baseR4";
+
+    //create Client
+    IGenericClient client = ctx.newRestfulGenericClient(baseUrl);
+    IParser jsonParser = ctx.newJsonParser().setPrettyPrint(true);
+
+    client.registerInterceptor(new LoggingInterceptor());
+
+    // search for Patient "Smith"
+    Bundle bundle = client.search()
+        .forResource(Patient.class)
+        .where(Patient.NAME.matches().value("Smith"))
+        .returnBundle(Bundle.class)
+        .execute();
+
+    String s = jsonParser.encodeResourceToString(bundle);
+    System.out.println(s);
+
 
   }
 }
